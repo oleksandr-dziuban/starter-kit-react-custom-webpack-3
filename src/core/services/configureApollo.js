cfd301e995cc00b566b78ca8ac9251bc3dfa2ffa
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2018-08-17 16:14:45 +0300

    Yarn lock

--- src/core/services/configureApollo.js
+++ src/core/services/configureApollo.js
@@ -1,29 +1,15 @@
-import ApolloClient, { InMemoryCache, HttpLink, ApolloLink } from 'apollo-boost';
-import { setContext } from 'apollo-link-context';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 
 // Transport URI
 const uri = process.env.REACT_APP_GRAPHQL_ENDPOINT;
-
-// Configure options
-const middleware = (request, previousContext) => ({
-  headers: {
-    ...previousContext.headers,
-  },
-});
-
-// Configure transport
-const link = ApolloLink.from([
-  new setContext(middleware),
-  new HttpLink({ uri }),
-]);
 
 // Configure cache
 const cache = new InMemoryCache();
 
 // Configure client
 const client = new ApolloClient({
-  link,
+  uri,
   cache: cache.restore(window.__APOLLO_STATE__ || {}),
 });
 
