66bc114dea44a89226b1cf1bd9eb4a1a138912a7
Oleksandr Dziuban <oleksandr_dziuban@epam.com>
2018-08-17 14:53:06 +0300

    Migrated to Apollo Boost

--- src/core/services/configureApollo.js
+++ src/core/services/configureApollo.js
@@ -1,8 +1,6 @@
-import ApolloClient from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloLink } from 'apollo-link';
+import ApolloClient, { InMemoryCache, HttpLink, ApolloLink } from 'apollo-boost';
 import { setContext } from 'apollo-link-context';
+
 
 // Transport URI
 const uri = process.env.REACT_APP_GRAPHQL_ENDPOINT;
